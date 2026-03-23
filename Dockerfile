9496746b3fa825f3aedf345f0f714a4b6fbf8f5a
André Lademann <alademann@thinkport.digital>
2026-03-23 17:31:18 +0100

    ${msg}

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,5 @@
 FROM gitlab/gitlab-ce:18.10.0-ce.0
 
-COPY docker-entrypoint.sh /usr/local/bin/railway-gitlab-entrypoint
-RUN chmod +x /usr/local/bin/railway-gitlab-entrypoint
+COPY --chmod=0755 docker-entrypoint.sh /usr/local/bin/railway-gitlab-entrypoint
 
 CMD ["/usr/local/bin/railway-gitlab-entrypoint"]
--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,10 +2,10 @@
 set -euo pipefail
 
 PORT="${PORT:-8080}"
-HOST="${RAILWAY_PUBLIC_DOMAIN:-localhost}"
+HOST="${RAILWAY_PUBLIC_DOMAIN:-${RAILWAY_PRIVATE_DOMAIN:-localhost}}"
 ROOT_PASSWORD="${GITLAB_ROOT_PASSWORD:-}"
 
-omnibus="external_url 'http://${HOST}'; nginx['listen_port']=${PORT}; nginx['listen_https']=false; puma['worker_processes']=0; sidekiq['max_concurrency']=10;"
+omnibus="external_url 'http://${HOST}:${PORT}'; nginx['listen_port']=${PORT}; nginx['listen_https']=false; nginx['listen_addresses']=['0.0.0.0','[::]']; gitlab_workhorse['listen_network']='tcp'; gitlab_workhorse['listen_addr']='0.0.0.0:8181'; puma['worker_processes']=0; sidekiq['max_concurrency']=10;"
 
 if [ -n "$ROOT_PASSWORD" ]; then
   omnibus="${omnibus} gitlab_rails['initial_root_password']='${ROOT_PASSWORD}';"
@@ -13,4 +13,4 @@ fi
 
 export GITLAB_OMNIBUS_CONFIG="$omnibus"
 
-exec /assets/wrapper
+exec /assets/init-container
